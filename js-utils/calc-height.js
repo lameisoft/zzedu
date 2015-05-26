d93309b0071b457b173bb7a8dadb3c93dbee9c33
lameisoft <lameisoft@qq.com>
2015-05-26 13:06:29 +0800

    删除js文件夹

--- js-utils/calc-height.js
+++
@@ -1,7 +0,0 @@
-function setParentIframeHeight(doc,id){
-	alert("Here");
-	var cHeight = Math.max(doc.body.clientHeight, doc.documentElement.clientHeight)
-	var sHeight = Math.max(doc.body.scrollHeight, doc.documentElement.scrollHeight)
-	var height  = Math.max(cHeight, sHeight)
-	parent.document.getElementById(id).style.height=height+"px";
-}
